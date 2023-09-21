fefd01b22198eedfb59442554b23b86a3659b061
fbwest <dimawest@gmail.com>
2023-09-21 20:43:27 +0300

    add jQuery

---
+++ jquery.ts
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $("#content").html("<h1>Hello TS</h1>");
+});
+$(() => {
+    $("#alertBtn").click(() =>
+        { $("#content").html("<h2>vivivivivivivivivi</h2>"); });
+});
